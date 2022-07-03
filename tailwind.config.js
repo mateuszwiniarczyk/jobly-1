8fcc8d992f70cd99a545bc343491f52d54b4bd07
Mateusz Winiarczyk <mateuszwiniarczyk99@gmail.com>
2022-07-03 20:46:54 +0200

    fix: remove unnecessary box shadow - tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,15 +39,6 @@ module.exports = {
         800: 'var(--color-gray-800)',
       },
     },
-    boxShadow: {
-      DEFAULT: '0px -6px 20px rgba(13, 10, 25, 0.08)',
-      sm: '0px -15px 20px rgba(13, 10, 25, 0.06)',
-      md: '0px 3px 4px #DBDCDF',
-      lg: '0px 8px 12px #DBDCDF',
-      xl: '0px 12px 14px rgba(153, 155, 168, 0.18)',
-      '2xl': '0px 16px 18px rgba(153, 155, 168, 0.18)',
-      '3xl': '0px 20px 24px rgba(153, 155, 168, 0.18)',
-    },
   },
   plugins: [],
 };
