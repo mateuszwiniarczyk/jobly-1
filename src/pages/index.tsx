c6435bfe9f9bd8bd81c45a45ae9b55ab2da7c68c
Mateusz Winiarczyk <mateuszwiniarczyk99@gmail.com>
2022-07-07 03:35:55 +0200

    feat: add dark theme and the possibility to toggle theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
 import type { NextPage } from 'next';
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 const Home: NextPage = () => {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  // When mounted on client, now we can show the UI
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
+  return (
+    <div className='bg-gray-400 dark:bg-slate-300 dark:text-white'>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <span>The current theme is: {theme}</span>
+      <div>
+        <button onClick={() => setTheme('light')}>Light Mode</button>
+        <button onClick={() => setTheme('dark')}>Dark Mode</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
