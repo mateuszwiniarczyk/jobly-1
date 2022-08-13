b286934d2e7790f173d4a492c8034979ec38ba20
Mateusz Winiarczyk <mateuszwiniarczyk99@gmail.com>
2022-08-13 22:34:36 +0200

    feat: create basic auth layout

--- src/features/auth/components/Layout.tsx
+++ src/features/auth/components/Layout.tsx
@@ -1,21 +1,23 @@
-import ArrowBack from 'public/svg/arrow_back.svg';
+import Link from 'next/link';
+import ArrowBackIcon from 'public/svg/arrow_back.svg';
+import WorkIcon from 'public/svg/work.svg';
 
 type LayoutProps = { children: React.ReactNode };
 
 export const Layout = ({ children }: LayoutProps) => {
   return (
     <div className='min-h-screen bg-white dark:bg-slate-200'>
-      <nav className='sticky border-b border-slate-100 p-5'>
-        <ArrowBack />
-        <ul className='flex'>
-          <li>Nav</li>
-          <li>Nav</li>
-          <li>Nav</li>
-          <li>Nav</li>
-          <li>Nav</li>
-        </ul>
+      <nav className='sticky flex items-center justify-center border-b border-gray-700 py-5 px-7 dark:border-slate-100'>
+        <Link href='/'>
+          <a className='absolute bottom-1/2 left-5 translate-y-1/2'>
+            <ArrowBackIcon className='fill-slate-100' />
+          </a>
+        </Link>
+        <div className='flex h-10 w-10 items-center justify-center rounded-full bg-primary-200 p-2.5'>
+          <WorkIcon className='fill-white' />
+        </div>
       </nav>
-      <div className='pt-24'>{children}</div>
+      <div className='px-7 pt-24'>{children}</div>
     </div>
   );
 };
