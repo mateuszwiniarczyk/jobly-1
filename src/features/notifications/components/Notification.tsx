a8052e986ebcba3d487a2c565d630c04a2986127
Mateusz Winiarczyk <mateuszwiniarczyk99@gmail.com>
2022-08-28 04:15:06 +0200

    feat: add the option to dismiss notifications

--- src/features/notifications/components/Notification.tsx
+++ src/features/notifications/components/Notification.tsx
@@ -4,7 +4,12 @@ import ErrorCircleIcon from 'public/svg/error.svg';
 import InformationCircleIcon from 'public/svg/info.svg';
 import WarningCircleIcon from 'public/svg/warning.svg';
 
-import { Notification } from '@/features/notifications/notificationSlice';
+import { useAppDispatch } from '@/hooks/useAppStore';
+
+import {
+  dismissNotification,
+  Notification,
+} from '@/features/notifications/notificationSlice';
 
 const icons = {
   info: (
@@ -24,34 +29,36 @@ const icons = {
   ),
 };
 
-const Notification = ({ type, title, message }: Omit<Notification, 'id'>) => (
-  <div className='flex w-full flex-col items-center space-y-4 sm:items-end'>
-    <div className='pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-gray-500 ring-opacity-5 dark:bg-slate-300 dark:ring-black'>
-      <div className='p-4' role='alert' aria-label={title}>
-        <div className='flex items-start'>
-          <div className='flex-shrink-0'>{icons[type]}</div>
-          <div className='ml-3 w-0 flex-1 pt-0.5 dark:text-gray-300'>
-            <p className='text-sm font-medium'>{title}</p>
-            <p className='mt-1 text-sm'>{message}</p>
-          </div>
-          <div className='ml-4 flex flex-shrink-0'>
-            <button
-              className='inline-flex rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2'
-              onClick={() => {
-                // onDismiss(id);
-              }}
-            >
-              <span className='sr-only'>Close</span>
-              <CloseIcon
-                className='h-5 w-5 dark:fill-gray-300'
-                aria-hidden='true'
-              />
-            </button>
+const Notification = ({ type, title, message, id }: Notification) => {
+  const dispatch = useAppDispatch();
+
+  return (
+    <div className='flex w-full flex-col items-center space-y-4 sm:items-end'>
+      <div className='pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-gray-500 ring-opacity-5 dark:bg-slate-300 dark:ring-black'>
+        <div className='p-4' role='alert' aria-label={title}>
+          <div className='flex items-start'>
+            <div className='flex-shrink-0'>{icons[type]}</div>
+            <div className='ml-3 w-0 flex-1 pt-0.5 dark:text-gray-300'>
+              <p className='text-sm font-medium'>{title}</p>
+              <p className='mt-1 text-sm'>{message}</p>
+            </div>
+            <div className='ml-4 flex flex-shrink-0'>
+              <button
+                className='inline-flex rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2'
+                onClick={() => dispatch(dismissNotification(id))}
+              >
+                <span className='sr-only'>Close</span>
+                <CloseIcon
+                  className='h-5 w-5 dark:fill-gray-300'
+                  aria-hidden='true'
+                />
+              </button>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export { Notification };
